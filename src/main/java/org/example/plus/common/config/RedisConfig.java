2c3e60d94b20d506c8be3a835a8c6e30e02d8180
Lee Hanbi <hanbi6717@naver.com>
2026-02-25 05:50:05 +0900

    chore: Java 8에서 추가된 LocalDate, LocalDateTime을 커버하기 위한 설정

--- src/main/java/org/example/plus/common/config/RedisConfig.java
+++ src/main/java/org/example/plus/common/config/RedisConfig.java
@@ -1,5 +1,10 @@
 package org.example.plus.common.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,6 +19,23 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+
+        // Redis 시간을 나타내는 LocalDate, LocalDateTime을 바로 사용할 수 없음
+        // -> 임의 설정 추가 필요함
+        // 추가 Java 8에서 추가된 LocalDate, LocalDateTime을 커버하기 위한 설정
+        // ObjectMapper 커스터마이징
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .activateDefaultTyping(
+                        LaissezFaireSubTypeValidator.instance,
+                        ObjectMapper.DefaultTyping.NON_FINAL,
+                        JsonTypeInfo.As.PROPERTY
+                );
+
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(mapper);
+
 
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
